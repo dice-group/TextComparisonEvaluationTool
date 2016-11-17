36bf746a4540337ef913e96ed158eff433043c51
TTurke <tobias.turke@gmx.net>
2016-11-17 20:07:17 +0100

    structure edit

--- Engines/simpleTextProcessing/StanfordSegmentatorTokenizer.java
+++ Engines/simpleTextProcessing/StanfordSegmentatorTokenizer.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
+import Enums.Language;
 import IOContent.TextReader;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
