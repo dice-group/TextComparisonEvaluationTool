4646791a6686e5ffd4bd7de8bd39c6abbb15008d
Subadmin <Subadmin@139.18.8.176>
2017-02-09 11:50:06 +0100

    edit info

--- src/main/java/Engines/Test/StanfordParser/package-info.java
+++ src/main/java/Engines/Test/StanfordParser/package-info.java
@@ -1,10 +1,11 @@
 
 /**
- * The containing examples are gather from the following resources:
+ * The containing examples are gather, edited and joined from the following resources:
  * 1. https://sites.google.com/site/nirajatweb/home/technical_and_coding_stuff/using-stanford-dependency-parser-simple-coding-example
  * 2. https://github.com/stanfordnlp/CoreNLP/tree/master/src/edu/stanford/nlp/parser/nndep
  * 3. https://github.com/awaisathar/dependensee/blob/master/src/test/java/com/chaoticity/dependensee/MainTest.java
  * 4. https://github.com/stanfordnlp/CoreNLP/tree/master/src/edu/stanford/nlp/parser/nndep
+ * 5. http://www.programcreek.com/java-api-examples/index.php?source_dir=SCNTreeViewer-master/src/edu/nus/comp/nlp/stanford/UtilParser.java
  *
  */
 package Engines.Test.StanfordParser;
